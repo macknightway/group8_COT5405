bd868b4fb563c03da02e0e089a04560d0021893f
Justin MacKenzie <justinmackenzie@Justins-MacBook-Pro.local>
2018-09-18 22:23:27 -0400

    Programming_Assignment_1: Updated divide.py algorithm
    Divide.py implements multiple subtractions in a loop, which increments a
    quotient counter

--- divide.py
+++ divide.py
@@ -1,32 +1,77 @@
 
-def div(a, b):
-    #Set the Quotient to 0 (empty string '')
-    quotient = ''
+def div(divisor, dividend):
+    #Set the Quotient to 0
+    quotient = 0
 
-    divisor = a
-    dividend = b
+    divisor = align_msb(divisor)
+    dividend = align_msb(dividend)
 
-    while dividend >= divisor:
-
-        # If dividend >= divisor -> shift 1
-        quotient += '1'
+    while len(dividend) >= len(divisor):
+        if len(dividend) == len(divisor):
+            for i in range(len(dividend)):
+                dividend_bit = dividend[i]
+                divisor_bit = divisor[i]
+                if dividend_bit < divisor_bit:
+                    # We're done!
+                    return quotient
+                elif dividend_bit >= divisor_bit:
+                    continue
+        # If dividend >= divisor -> add 1
+        quotient += 1
 
         # Subtract divisor from dividend
-        dividend = sub(dividend, divisor)
+        dividend = align_msb(sub(dividend, divisor))
 
-        # Shift divisor -> right one
-        dividend = dividend[1:]
+    return quotient
 
-    else:
-        # If dividend < divisor -> shift 0
-        quotient += '0'
-        break
+def align_msb(bin_data):
+    length = len(bin_data)
+    for i in range(length):
+        if bin_data[0] != '1':
+            #Shift <- left
+            bin_data = bin_data[1:]
+        else:
+            break
+    return bin_data
+
+def bit_flip(bin_data):
+    flipped_data = ''
+    for i in range(len(bin_data)):
+        if bin_data[-(i+1)] == '0':
+            flipped_data = '1' + flipped_data
+        else:
+            if (i+1) == len(bin_data):
+                # We are at the msb
+                flipped_data = '0' + flipped_data
+            else:
+                # We are not at the msb (make sure to keep all leftmost bits)
+                flipped_data = bin_data[:-(i+1)] + '0' + flipped_data
+                break
+    return flipped_data
 
 def sub(dividend, divisor):
-    pass
-
+    # Dividend: 1011 -> 11
+    # Divisor:   100 -> 4
+    # Answer: 111 -> 7
+    result = ''
+    length = len(dividend)
+    for i in range(length - len(divisor)):
+        divisor = '0' + divisor
+    for i in range(length):
+        if dividend[-1] == '1' and divisor[-1] == '0':
+            result = '1' + result
+        elif dividend[-1] == '0' and divisor[-1] == '0':
+            result = '0' + result
+        elif dividend[-1] == '1' and divisor[-1] == '1':
+            result = '0' + result
+        elif dividend[-1] == '0' and divisor[-1] == '1':
+            #Note that for us to be in this function there must exist
+            #at least one 1 in the dividend string to borrow from
+            dividend = bit_flip(dividend)
+            result = '1' + result
+        dividend = dividend[:-1]
+        divisor = divisor[:-1]
+    return result
 
 if __name__ == '__main__':
-    a = 10
-    b = 01
-    #div(a, b)
+    print(div('10', '10000000'))
