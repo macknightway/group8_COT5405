21abd469036b46433c4e41bd7356a8364aece6c7
Justin MacKenzie <justinmackenzie@Justins-MacBook-Pro.local>
2018-09-19 21:07:39 -0400

    Programming_Assignment_1: Added 2s complement functionality and bug fixes

--- divide.py
+++ divide.py
@@ -2,7 +2,11 @@
 def div(divisor, dividend):
     #Set the Quotient to 0
     quotient = 0
-
+    is_negative = not (divisor[0] == dividend[0])
+    if divisor[0] == '1':
+        divisor = twos_complement(divisor)
+    if dividend[0] == '1':
+        dividend = twos_complement(dividend)
     divisor = align_msb(divisor)
     dividend = align_msb(dividend)
 
@@ -14,8 +18,11 @@ def div(divisor, dividend):
                 if dividend_bit < divisor_bit:
                     # We're done!
                     return quotient
-                elif dividend_bit >= divisor_bit:
+                elif dividend_bit == divisor_bit:
                     # Not done yet!
+                    continue
+                elif dividend_bit > divisor_bit:
+                    # We can keep dividing
                     break
         # If dividend >= divisor -> add 1
         quotient += 1
@@ -23,7 +30,11 @@ def div(divisor, dividend):
         # Subtract divisor from dividend
         dividend = align_msb(sub(dividend, divisor))
 
-    return quotient
+    if is_negative:
+        return -1 * quotient
+    else:
+        return quotient
+
 
 def align_msb(bin_data):
     length = len(bin_data)
@@ -34,6 +45,7 @@ def align_msb(bin_data):
         else:
             break
     return bin_data
+
 
 def bit_flip(bin_data):
     flipped_data = ''
@@ -49,6 +61,7 @@ def bit_flip(bin_data):
                 flipped_data = bin_data[:-(i+1)] + '0' + flipped_data
                 break
     return flipped_data
+
 
 def sub(dividend, divisor):
     # Dividend: 1011 -> 11
@@ -74,16 +87,96 @@ def sub(dividend, divisor):
         divisor = divisor[:-1]
     return result
 
+
+def add_one(bin_data):
+    result = ''
+    length = len(bin_data)
+    for i in range(length):
+        if bin_data[-1] == '0':
+            result = bin_data[:-1] + '1'
+            break
+        if bin_data[-1] == '1':
+            i = -1
+            while bin_data[i] == '1':
+                result = '0' + result
+                i -= 1
+            else:
+                if (abs(i) == length):
+                    result = '1' + result
+                else:
+                    result = bin_data[:-i] + '1' + result
+            break
+    return result
+
+
+def twos_complement(bin_data):
+    twos_complement = ''
+    for bin_digit in bin_data:
+        if bin_digit == '0':
+            twos_complement += '1'
+        elif bin_digit == '1':
+            twos_complement += '0'
+    return add_one(twos_complement)
+
+
 if __name__ == '__main__':
     import json
 
     with open('inputs.json') as json_data:
+        #x = div('0' + bin(2829)[2:], '0' + bin(23833)[2:])
+        #y = 84861 / 92185
         data = json.load(json_data)
         sizes = data.keys()
         #for size in sizes:
         size = 'size_16'
         divisors = data[size]['divisor']
         dividends = data[size]['dividend']
+        divisor_neg = False
+        dividend_neg = False
+        false_flag = False
         for divisor in divisors:
+            if divisor[2] == '1':
+                divisor_neg = True
+                printed_divisor = twos_complement(divisor)
+            else:
+                printed_divisor = divisor
             for dividend in dividends:
-                print('{0} divided by {1} = {2}'.format(int(dividend,2), int(divisor,2), div(divisor, dividend)))
+                if dividend[2] == '1':
+                    dividend_neg = True
+                    printed_dividend = twos_complement(dividend)
+                else:
+                    printed_dividend = dividend
+                if divisor_neg and dividend_neg:
+                    x = div(divisor, dividend)
+                    print('-{0} divided by -{1} = {2}'.format(int(printed_dividend, 2),
+                                                            int(printed_divisor, 2),
+                                                            x))
+                    y = int(int(dividend, 2) / int(divisor, 2))
+                    if not (x == y):
+                        false_flag = True
+                elif divisor_neg and not dividend_neg:
+                    x = div(divisor, dividend)
+                    print('{0} divided by -{1} = -{2}'.format(int(printed_dividend, 2),
+                                                            int(printed_divisor, 2),
+                                                            x))
+                    y = int(int(dividend, 2) / int(divisor, 2))
+                    if not (x == y):
+                        false_flag = True
+                elif not divisor_neg and dividend_neg:
+                    x = div(divisor, dividend)
+                    print('-{0} divided by {1} = -{2}'.format(int(printed_dividend, 2),
+                                                            int(printed_divisor, 2),
+                                                            x))
+                    y = int(int(dividend, 2) / int(divisor, 2))
+                    if not (x == y):
+                        false_flag = True
+                else:
+                    x = div(divisor, dividend)
+                    print('{0} divided by {1} = {2}'.format(int(printed_dividend,2),
+                                                            int(printed_divisor,2),
+                                                            x))
+                    y = int(int(dividend,2) / int(divisor,2))
+                    if not (x == y):
+                        false_flag = True
+        if not false_flag:
+            print('All TEST CASES PASSED')
